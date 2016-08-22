ef6731347fff4bf7bb54a116817ccec61ee3ac18
stonedong <yishuiliunian@gmail.com>
2016-08-23 00:08:27 +0800

    init

---
+++ DZPinyin/Classes/DZPinyin.h
@@ -0,0 +1,11 @@
+//
+//  DZPinyin
+//
+//  Created by mac on 15/8/27.
+//  Copyright (c) 2015年 kaicheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+char pinyinFirstLetter(unsigned short hanzi);
